634b86c0ead325be6b8814685109bb757d6cf9df
Hayden <77524675+casesDx11@users.noreply.github.com>
2025-12-02 11:43:29 -0600

    Install prettier and formatted files

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import MyPascalCaseComponent from './MyPascalCaseComponent.tsx'
-import Card from './Card.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import MyPascalCaseComponent from "./MyPascalCaseComponent.tsx";
+import Card from "./Card.tsx";
 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Card />
     <MyPascalCaseComponent />
     <App />
   </StrictMode>,
-)
+);
