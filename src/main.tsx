8335923e2f48547a7411517121f94d252c63cd69
Hayden <77524675+casesDx11@users.noreply.github.com>
2025-12-02 10:59:59 -0600

    Created a card component

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import MyPascalCaseComponent from './MyPascalCaseComponent.tsx'
+import Card from './Card.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Card />
     <MyPascalCaseComponent />
     <App />
   </StrictMode>,
